2ee0c1232836eda0b89835a4a9b5b2860181b1e2
Anwar3006 <anwarsadat.d2@gmail.com>
2023-01-12 01:20:51 +0000

    IT Automation

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file in /tmp
 
 file { 'school':
-  ensure => 'present',
+  ensure  => 'present',
   content => 'I love Puppet',
-  owner => 'www-data',
-  mode => '0744',
-  group => 'www-data'
+  owner   => 'www-data',
+  mode    => '0744',
+  group   => 'www-data'
 }
