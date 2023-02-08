c6da675a5314aadbe715d52cdfd48255bdedff88
Anwar3006 <anwarsadat.d2@gmail.com>
2023-02-08 14:09:36 +0000

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" """
+import requests
+import sys
+
+req_name = requests.get('https://jsonplaceholder.typicode.com/users/' + sys.argv[1])
+req_tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1])
+
+res_name = req_name.json()['name']
+tasks = req_tasks.json()
+
+completed_tasks = 0
+total_tasks = 0
+completed_tasks_list =[]
+
+for task_done in tasks:
+    if task_done["completed"] is True:
+        completed_tasks += 1
+        total_tasks += 1
+        completed_tasks_list.append(task_done)
+    else:
+        total_tasks += 1
+
+
+print("Employee {} is done with tasks"
+    " ({}/{}):".format(res_name, completed_tasks, total_tasks))
+
+for task_done in completed_tasks_list:
+    print("\t {}".format(task_done["title"]))
+
